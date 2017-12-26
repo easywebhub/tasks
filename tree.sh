a7ad6cebb95e560c3545562d119c03fcafa389e0
quocbao_home <quocbao.tn@gmail.com>
2017-12-26 11:36:22 +0700

    tóm gọn mô hình

--- tree.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# bash script to generate tree structure of a directory
-# Pravendra Singh (@hackpravj)
-
-pwd=$(pwd)
-find $pwd -print | sed -e "s;$pwd;\.;g;s;[^/]*\/;|--;g;s;--|; |;g"
